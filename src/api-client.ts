319779f381fbad4e0bd9aee795ae1fffeb949392
utgwkk <utagawakiki@gmail.com>
2022-04-28 01:42:17 +0900

    set User-Agent

--- src/api-client.ts
+++ src/api-client.ts
@@ -2,4 +2,7 @@ import { youtube_v3 } from "@googleapis/youtube";
 
 export const client = new youtube_v3.Youtube({
   auth: process.env.API_KEY,
+  userAgentDirectives: [
+    { product: "youtube-data-graphql-api", version: "0.0.1" },
+  ],
 });
