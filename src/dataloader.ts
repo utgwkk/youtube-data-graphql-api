f7f0469d6b48da015abcab42a7acdb0ec58c6710
utgwkk <utagawakiki@gmail.com>
2022-04-28 01:35:11 +0900

    nullable

--- src/dataloader.ts
+++ src/dataloader.ts
@@ -3,19 +3,20 @@ import DataLoader from "dataloader";
 
 import { client as apiClient } from "./api-client";
 
-export const videoLoader = new DataLoader<string, youtube_v3.Schema$Video>(
-  async (keys) => {
-    console.debug("batch load videos");
-    const { data } = await apiClient.videos.list({
-      id: keys.slice(),
-      part: ["snippet", "contentDetails"],
-    });
+export const videoLoader = new DataLoader<
+  string,
+  youtube_v3.Schema$Video | null
+>(async (keys) => {
+  console.debug("batch load videos");
+  const { data } = await apiClient.videos.list({
+    id: keys.slice(),
+    part: ["snippet", "contentDetails"],
+  });
 
-    if (!data.items) {
-      return [];
-    }
-
-    const itemByKey = new Map(data.items.map((item) => [item.id!, item]));
-    return keys.map((key) => itemByKey.get(key)!);
+  if (!data.items) {
+    return [];
   }
-);
+
+  const itemByKey = new Map(data.items.map((item) => [item.id!, item]));
+  return keys.map((key) => itemByKey.get(key) ?? null);
+});
